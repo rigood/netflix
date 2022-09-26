b08c2d621ba096137815a8f6898a3c229e0a0597
rigood <rigood@naver.com>
2022-09-26 16:37:51 +0900

    💄 Change Modal positioning

--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -165,6 +165,7 @@ const Overlay = styled(motion.div)`
 const BigMovie = styled(motion.div)<{ scrolly: number }>`
   position: fixed;
   top: 0;
+  bottom: 0;
   left: 0;
   right: 0;
   margin: auto;
